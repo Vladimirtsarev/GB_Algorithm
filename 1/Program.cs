314ed5166e7c64f3429ecbd59527514174f72a3e
Владимир <79262685273@ya.ru>
2019-03-11 15:48:41 +0300

    Задача 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -21,8 +21,71 @@ namespace _1
 {
     class Program
     {
+        static int N = 4;
+        static int M = 3;
+
         static void Main(string[] args)
         {
+            
+            int[,] A = new int[N, M];
+            int[,] Map =  { { 3, 3, 3 }, 
+                            { 1, 1, 1 },
+                            { 0, 1, 0 },
+                            { 0, 1, 0 } }; //Карта
+            Console.WriteLine("Карта:");
+            Print2(4, 3, Map);
+            //Console.WriteLine("\n");
+            int i, j;
+            for (j = 0; j < M; j++)
+                if (Map[0, j] >= 1)
+                {
+                    A[0, j] = 1;    // Первая строка заполнена единицами
+                } 
+            for (i = 1; i < N; i++)
+            {
+                if (Map[i, 0] >= 1)
+                {
+                    A[i, 0] = 1;
+                }
+                for (j = 1; j < M; j++)
+                {
+                    if (Map[i, j] >= 1)
+                    {
+                        A[i, j] = A[i, j - 1] + A[i - 1, j];
+                    }
+
+                    if (Map[i, j] == 0)
+                    {
+                        A[i, j] = 0;
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Количество маршрутов:");
+            Print2(N, M, A);
+            Console.WriteLine("\nНажмите эникей");
+            Console.ReadKey();
+
         }
+
+        /// <summary>
+        /// Вывод массива
+        /// </summary>
+        /// <param name="n">Число столбцов</param>
+        /// <param name="m">Число строк</param>
+        /// <param name="a">Массив</param>
+        static void Print2(int n, int m, int[,] a)
+        {
+            int i, j;
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                    Console.Write(a[i, j] + " ");
+                Console.Write("\n");
+            }
+            //Console.ReadKey();
+        }
+
     }
 }
